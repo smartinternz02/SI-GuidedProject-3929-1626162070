aa03e14cce3e375781c91b269369d3c58c278001
KavishMehta08 <kavishmehta8@gmail.com>
2021-07-25 02:26:44 +0400

    ASSIGNMENT-6

---
+++ ASSIGNMENT-6/new_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/kavish/new_ws/src
+# Build directory: /home/kavish/new_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("pysdf")
+subdirs("mybot_gazebo")
+subdirs("mybot_description")
---
+++ ASSIGNMENT-6/new_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/kavish/new_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/kavish/new_ws/devel;/home/kavish/Bot_ws/devel;/home/kavish/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/kavish/new_ws/devel;/home/kavish/Bot_ws/devel;/home/kavish/catkin_ws/devel;/opt/ros/melodic' != '' else []
